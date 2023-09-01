a1767d9c0e831e43b5e7b58060688c6334e332ea
getch <getahunlast@gmail.com>
2023-09-01 14:28:01 -0700

    computer typing effet added

--- Client/src/Components/Chatbox/chatbox.jsx
+++ Client/src/Components/Chatbox/chatbox.jsx
@@ -1,88 +1,123 @@
-import styles from './chatbox.module.css';
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import styles from "./chatbox.module.css"; // Import styles correctly
 
-import Welcome from './Welcome/welcome';
-import TypingAnimation from './typingAnim/TypingAnimation';
+import Welcome from "./Welcome/welcome";
+import TypingAnimation from "./typingAnim/TypingAnimation";
 
 const Chatbox = () => {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const autoScrolled = useRef(null);
+  const [typingAnimation, setTypingAnimation] = useState(false);
+  const [completedTyping, setCompletedTyping] = useState(false);
+  const [displayResponse, setDisplayResponse] = useState("");
 
-    const [input, setInput] = useState("");
-    const [messages, setMessages] = useState([]);
-    const autoScrolled = useRef(null);
-    const [typingAnimation ,setTyping] = useState(false)
-    
+  const handleSend = async (e) => {
+    if (e) {
+      e.preventDefault();
+    }
 
-    const handleSend = async (e) => {
+    console.log("Input: ", input);
+
+    try {
+      const response = await fetch("http://localhost:3000/ask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ question: input }),
+      });
+
+
+      if (!response.ok) {
+        throw new Error("Network response was not okay");
+      }
+
+      const data = await response.json();
       
+      setTypingAnimation(true)
 
-        if (e){
-            e.preventDefault();
-        }
-        
-        console.log('From input: ', input);
-      
-        try {
-          const response = await fetch('http://localhost:3000/ask', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ question: input })
-          });
-          setTyping(true);
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-      
-          const data = await response.json();
-          setMessages([
-            ...messages,
-            { text: input, isUser: true },
-            { text: data.answer, isUser: false }
-          ]);
-          setInput("");
-          setTyping(false);
-          console.log(data.answer);
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      };
+      let i = 0;
+      const chatHistory =data.answer;
 
-        useEffect(() => {
-          // Scroll to the bottom of the container when component updates
-          autoScrolled.current.scrollTop = autoScrolled.current.scrollHeight;
-        });
+      const intervalId = setInterval(() => {
+        setDisplayResponse(chatHistory.slice(0, i));
 
+        i++;
+
+        if (i > chatHistory.length) {
+          clearInterval(intervalId);
+          setTypingAnimation(false)
          
-     
-    return (
-        <section className={styles.chatbox} ref={autoScrolled}>
           
-            <Welcome handleSend={handleSend} input={input} setInput={setInput} setMessages={setMessages}/>
+        }
+      }, 20);
+      
+      setMessages([
+        ...messages,
+        { text: input, isUser: true },
+        { text: data.answer, isUser: false },
+      ]);
 
-            <div className={styles.chatWrapper}>
+      setInput("");
+      console.log(data.answer);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-             {messages.map((message, index) => (
-                <div key={index} 
-                className={message.isUser? styles.chatLogUser : styles.chatLogAi}> 
-                <div className={message.isUser? styles.chatMessageUser : styles.chatMessageAi}>
-                    <div className={message.isUser? styles.avatarUser : styles.avatarAi}></div>
-                    <div className={message.isUser? styles.messageUser : styles.messageAi}>{message.text}</div>
-                </div>    
-            </div>
-            ))}
-            
-            </div>
-            
-            <div className={styles.chatInputholder}>
-                {typingAnimation ? <TypingAnimation /> : null}
-                <input placeholder='Ask me anything ...' className={styles.chatInputTextarea} value={input} onChange={(e) => setInput(e.target.value)}></input>
-                <button onClick={handleSend} className={input !== "" ? styles.sendBtn : null} disabled = {input === ''}>send</button>
-               
+  useEffect(() => {
+    // Scroll to the bottom of the container when component updates
+    autoScrolled.current.scrollTop = autoScrolled.current.scrollHeight;
+  }, [messages]); // Add dependency array to useEffect
+
+  return (
+    <section className={styles.chatbox} ref={autoScrolled}>
+      <Welcome
+        handleSend={handleSend}
+        input={input}
+        setInput={setInput}
+        setMessages={setMessages}
+      />
+
+   
+
+      <div className={styles.chatWrapper}>
+        {messages.map((message, index) => (
           
+          <div  key={index} className={message.isUser ? styles.chatLogUser : styles.chatLogAi} >
+            
+          
+            <div className={message.isUser ? styles.chatMessageUser : styles.chatMessageAi}>
+             <div className={message.isUser ? styles.avatarUser : styles.avatarAi} ></div>
+               <div className={ message.isUser ? styles.messageUser : styles.messageAi}>
+
+                 {index === messages.length - 1 ? (message.isUser ? message.text : displayResponse ) : ( // Check if it's the last message
+                 message.text )} 
+               </div>  
             </div>
-        </section>
-    )
-}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.chatInputholder}>
+       <div style={{with:"30%"}}> {typingAnimation ? <TypingAnimation /> : null}</div>
+        <input
+          placeholder="Ask me anything ..."
+          className={styles.chatInputTextarea} // Use the correct CSS class
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <button
+          onClick={handleSend}
+          className={input !== "" ? styles.sendBtn : null}
+          disabled={input === ""}
+        >
+          Send
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default Chatbox;
--- Client/src/Components/Chatbox/typingAnim/TypingAnimation.jsx
+++ Client/src/Components/Chatbox/typingAnim/TypingAnimation.jsx
@@ -5,11 +5,11 @@ import "./typing.css";
 function TypingAnimation(){
 
     return(
-        <div class="newtons-cradle">
-         <div class="newtons-cradle__dot"></div>
-         <div class="newtons-cradle__dot"></div>
-          <div class="newtons-cradle__dot"></div>
-        <div class="newtons-cradle__dot"></div>
+        <div className="newtons-cradle">
+         <div className="newtons-cradle__dot"></div>
+         <div className="newtons-cradle__dot"></div>
+          <div className="newtons-cradle__dot"></div>
+        <div className="newtons-cradle__dot"></div>
         </div>
 
     )
