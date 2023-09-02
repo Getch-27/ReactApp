de26a9c3810d776ae3c4d7597b1ff55c033df5db
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2023-09-01 20:50:47 -0700

    changed the directory structure

--- Client/src/Components/Aside/aside.jsx
+++ Client/src/Components/Aside/aside.jsx
@@ -3,7 +3,7 @@ import styles from './aside.module.css'
 const Aside = () => {
     return (
         <aside className={styles.aside}>
-            <h1>Aside</h1>
+            <h1></h1>
         </aside>
     )
 }
