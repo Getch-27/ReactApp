2d3e2ff4eb235ab8bc2ab06600e48cf9e29a28a3
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2023-09-01 21:24:15 -0700

    added chat features

---
+++ Client/src/Components/Chatbox/chatbox.jsx
@@ -0,0 +1,78 @@
+import styles from './chatbox.module.css';
+import { useState, useEffect, useRef } from 'react';
+import Welcome from './Welcome/welcome';
+
+const Chatbox = () => {
+
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([]);
+    const autoScrolled = useRef(null);
+    
+
+    const handleSend = async (e) => {
+        e.preventDefault();
+        console.log('From input: ', input);
+      
+        try {
+          const response = await fetch('http://localhost:3000/ask', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ question: input })
+          });
+      
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+      
+          const data = await response.json();
+          setMessages([
+            ...messages,
+            { text: input, isUser: true },
+            { text: data.answer, isUser: false }
+          ]);
+          setInput("");
+          console.log(data.answer);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+
+        useEffect(() => {
+          // Scroll to the bottom of the container when component updates
+          autoScrolled.current.scrollTop = autoScrolled.current.scrollHeight;
+        });
+
+
+
+    return (
+        <section className={styles.chatbox} ref={autoScrolled}>
+            
+            <Welcome />
+
+            <div className={styles.chatWrapper}>
+
+             {messages.map((message, index) => (
+                <div key={index} 
+                className={message.isUser? styles.chatLogUser : styles.chatLogAi}> 
+                <div className={message.isUser? styles.chatMessageUser : styles.chatMessageAi}>
+                    <div className={message.isUser? styles.avatarUser : styles.avatarAi}></div>
+                    <div className={message.isUser? styles.messageUser : styles.messageAi}>{message.text}</div>
+                </div>    
+            </div>
+            ))}
+            
+            </div>
+
+            <div className={styles.chatInputholder}>
+                
+                <input placeholder='Ask me anything ...' className={styles.chatInputTextarea} value={input} onChange={(e) => setInput(e.target.value)}></input>
+                <button onClick={handleSend} className={styles.sendBtn}>Send</button>
+
+            </div>
+        </section>
+    )
+}
+
+export default Chatbox;
