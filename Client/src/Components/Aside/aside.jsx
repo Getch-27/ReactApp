32105f8991f941e6dcf23cf5dd8b5e1584e1d12f
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2023-09-01 19:53:17 -0700

    created a sidebar

---
+++ Client/src/Components/Aside/aside.jsx
@@ -0,0 +1,11 @@
+import styles from './aside.module.css'
+
+const Aside = () => {
+    return (
+        <aside className={styles.aside}>
+            <h1>Aside</h1>
+        </aside>
+    )
+}
+
+export default Aside;
