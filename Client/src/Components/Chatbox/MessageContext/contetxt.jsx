d1ffc77565a03593c8d72e4b3cfbee7ed7d9aa63
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2023-09-03 20:09:16 -0700

    conflict resolved

--- Client/src/Components/Chatbox/MessageContext/contetxt.jsx
+++ Client/src/Components/Chatbox/MessageContext/contetxt.jsx
@@ -14,16 +14,9 @@ function MessageContext(props){
   }
   return (
     <div>
-<<<<<<< HEAD
       {/* Renders a ReadMoreIcon component with an onClick event */}
       <ReadMoreIcon onClick={HandleClick} />
       {/* Renders a paragraph element with the display context based on the state */}
-=======
-      <div className="ContectButton">
-         <h3>Show Context</h3>
-         <ReadMoreIcon fontSize="large" onClick={HandleClick} />
-      </div>
->>>>>>> 623305a3479b6d62105073c7ce971fc29c74159b
       <p style={{ display: displayContext  }}>
         {props.msgContext}
       </p>
