e21c8a54791a569f881746aab9530f4327867270
getch <getahunlast@gmail.com>
2023-09-23 14:58:13 -0700

    add blur effect

--- Client/src/Components/PlatformSection/Platform.jsx
+++ Client/src/Components/PlatformSection/Platform.jsx
@@ -1,7 +1,7 @@
 
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef } from "react";
-import TelegramImage from "../../assets/images/telegram.png"
+import TelegramImage from "../../assets/images/telegramBot.png"
 import AndroidImage from "../../assets/images/android.jpg"
 import Button from "../Button";
 
@@ -14,8 +14,8 @@ const HorizontalScrollCarousel = () => {
   const x = useTransform(scrollYProgress, [0, 1], ["0%", "-50%"]);
 
   return (
-    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
-      <div className="sticky top-0 flex h-screen items-center overflow-hidden bg-primary">
+    <section ref={targetRef} className="relative h-[300vh] bg-neutral-900  snap-mandatory snap-x">
+      <div className="sticky top-0 flex h-screen items-center overflow-hidden bg-primary snap-center ">
         <motion.div  style={{ x }} className="flex gap-4  ">
           {cards.map((card) => {
             return <Card card={card} key={card.id} />;
@@ -28,26 +28,37 @@ const HorizontalScrollCarousel = () => {
 
 const Card = ({ card }) => {
   return (
-    <div key={card.id} className="group relative  h-[450px] w-[460px]  md:w-[1470px]  " >
-      <div className="absolute inset-0 z-10 grid place-content-center  md:w-128 bg-metal  element">
-      <img src={card.url} className=" h-64  rounded-lg"></img>
-
-<motion.div 
- initial={{opacity:0 ,scale : 0}}
- whileInView={{ opacity: 1 , scale : 1 }}
- viewport={{ once: true }}
- transition={{ delay: 0.3,
-                 duration : 0.5
-  }}
- className=" bg-white w-3/4 p-6   h-64  rounded-lg flex flex-col justify-center items-center">
- <h1 className="font-martian text-metal text-xl m-4">{card.title}</h1>
- <h3 className="font-inter mb-4 text-secondary ">{card.heading}</h3>
- <p className="font-inter">{card.pahragraph}</p>
- <Button
-     text="START CHAT"
-     styles="mt-8  bg-gradient-to-r from-midnight to-metal rounded-2xl  border-metal font-bold px-6 py-3 text-white "
-   />
-</motion.div>
+    <div
+      key={card.id}
+      className="group relative  h-[450px] w-[460px]  md:w-[1470px]  "
+    > 
+      <div className="absolute inset-0 z-10 grid place-content-center  md:w-128 w-80 ">
+        <div
+          style={{
+            backgroundImage: `url(${card.url})`,
+            backgroundSize: "",
+            backgroundPosition: "center",
+          }}
+          className="absolute inset-0 z-0 transition-transform duration-300 flex bg-no-repeat"
+        >
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className=" bg-white/10 z-10 backdrop-filter backdrop-blur-lg shadow-lg w-3/4 p-6 h-80 m-auto m-76 rounded-lg flex flex-col justify-center items-center"
+          >
+            <h1 className="font-martian text-metal text-xl m-4">
+              {card.title}
+            </h1>
+            <h3 className="font-inter mb-4 text-secondary ">{card.heading}</h3>
+            <p className="font-inter">{card.pahragraph}</p>
+            <Button
+              text="START CHAT"
+              styles="mt-8  bg-gradient-to-r from-midnight to-metal rounded-2xl  border-metal font-bold px-6 py-3 text-white "
+            />
+          </motion.div>
+        </div>
       </div>
     </div>
   );
